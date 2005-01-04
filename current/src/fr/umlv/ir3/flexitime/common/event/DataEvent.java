6c93a58a1d44b5226c9dfc38bd600cd29e380478
jguers <jguers@42224c30-ede5-0310-ac12-a7a1f9b436d4>
2005-01-04 17:00:22 +0000

    Ajout de DataEvent

---
+++ current/src/fr/umlv/ir3/flexitime/common/event/DataEvent.java
@@ -0,0 +1,26 @@
+/*
+ * Created on 30 déc. 2004 by Jérôme GUERS Copyright: GPL - UMLV(FR) - 2004/2005
+ */
+
+package fr.umlv.ir3.flexitime.common.event;
+
+import fr.umlv.ir3.flexitime.common.data.Data;
+
+/**
+ * DOCME Je ne sais pas à quoi cela sert! C'est juste pour ne plus avoir
+ * d'erreur à la compilation
+ * 
+ * @author FlexiTeam - Jérôme GUERS
+ */
+public class DataEvent
+{
+
+    /**
+     * DOCME
+     */
+    public DataEvent(Data d, int i)
+    {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+}
