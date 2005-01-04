c073329b5c2d8aac18194357e4fb1f8a2a38dee2
binou <binou@42224c30-ede5-0310-ac12-a7a1f9b436d4>
2005-01-04 18:27:38 +0000

    Premier test pour l'API d'EDT ... Création d'un environnement générale de l'appli avec le composant au milieu Composant encore vide

---
+++ members/fappert/src/fr/umlv/ir3/GL/test/edt/FlexiEDT.java
@@ -0,0 +1,27 @@
+/*
+ * Created on 4 janv. 2005
+ * by binou
+ * Copyright: GPL - UMLV(FR) - 2004/2005
+ */
+package fr.umlv.ir3.GL.test.edt;
+
+import javax.swing.JPanel;
+
+
+/**
+ * DOCME Description
+ * explication supplémentaire si nécessaire
+ * in english please...
+ * Que fait cette classe, qu'est-ce qu'elle 
+ * représente, ...
+ * 
+ * @version Verion ou révision SVN
+ * @see (si nécessaire)
+ * 
+ * @author FlexiTeam - binou
+ */
+public class FlexiEDT extends JPanel
+{
+
+}
+
