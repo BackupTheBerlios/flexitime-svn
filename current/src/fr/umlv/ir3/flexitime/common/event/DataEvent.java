b994e74e7f73fc10732653ca47b1748822844040
pramassa <pramassa@42224c30-ede5-0310-ac12-a7a1f9b436d4>
2005-01-06 13:03:45 +0000

    Dev de DataEvent Re car suppression après MAJ AB

--- current/src/fr/umlv/ir3/flexitime/common/event/DataEvent.java
+++ current/src/fr/umlv/ir3/flexitime/common/event/DataEvent.java
@@ -4,23 +4,70 @@
 
 package fr.umlv.ir3.flexitime.common.event;
 
+import java.io.Serializable;
+import java.util.EventObject;
+
 import fr.umlv.ir3.flexitime.common.data.IData;
 
 /**
- * DOCME Je ne sais pas à quoi cela sert! C'est juste pour ne plus avoir
- * d'erreur à la compilation
+ * Event sended to IDataListener
+ * 
+ * @see fr.umlv.ir3.flexitime.common.rmi.IDataListener
  * 
  * @author FlexiTeam - Jérôme GUERS
  */
-public class DataEvent
+public class DataEvent extends EventObject implements Serializable
 {
 
     /**
-     * DOCME
+     * 
+     * Event delivered when the state of a IData has changed
+     * @param d IData that originated the event
+     * @param eType one of TYPE_PROPERTY_CHANGED, TYPE_PROPERTY_ADDED
+     * 		or TYPE_PROPERTY_REMOVED.
      */
-    public DataEvent(IData d, int i)
+    public DataEvent(IData d, int eType)
     {
-        super();
-        // TODO Auto-generated constructor stub
+        super(d);
+        eventType = eType;
+        
     }
+    
+    /**
+     * Return eventType
+     * @return Returns the eventType.
+     */
+    public int getEventType()
+    {
+        return eventType;
+    }
+    
+    /**
+     *  Getter for IData contained in this event
+     * 
+     * @return IData that generated this event 
+     * 
+     */
+    public IData getData()
+    {
+        return (IData)getSource();
+    }
+    
+    private final int eventType;
+    
+    /**
+     * indicates that the data has changed
+     */
+    public final static int TYPE_CHANGED=1;
+    
+    /**
+     * Indicates that the data has been added added
+     */
+    public final static int TYPE_ADDED=2;
+    
+    /**
+     * Indicates that the data has been removed
+     */
+    public final static int TYPE_REMOVED=3;
+    
 }
