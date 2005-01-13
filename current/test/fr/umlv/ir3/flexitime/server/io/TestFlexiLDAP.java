9c4cdebce7bc730858cdb3931b5d597593b3eb6d
gguerrin <gguerrin@42224c30-ede5-0310-ac12-a7a1f9b436d4>
2005-01-13 11:28:35 +0000

    Livraison de TestFlexiLDAP

---
+++ current/test/fr/umlv/ir3/flexitime/server/io/TestFlexiLDAP.java
@@ -0,0 +1,45 @@
+/*
+ * Created on 13 janv. 2005
+ * by Famille
+ * Copyright: GPL - UMLV(FR) - 2004/2005
+ */
+package fr.umlv.ir3.flexitime.server.io;
+
+import java.util.ArrayList;
+
+import junit.framework.TestCase;
+
+
+/**
+ * DOCME Description
+ * explication supplémentaire si nécessaire
+ * in english please...
+ * Que fait cette classe, qu'est-ce qu'elle 
+ * représente, ...
+ * 
+ * @version Verion ou révision SVN
+ * 
+ * @author FlexiTeam - Famille
+ */
+public class TestFlexiLDAP extends TestCase
+{
+    public void testCreateConnection()
+    {
+        String name="fappert";
+        String passwd = "";
+        FlexiLDAP ldap = new FlexiLDAP("gontime.ath.cx",389,"ou=Users,ou=Etudiant,dc=donvincenzo,dc=com","ou=Groups,ou=Etudiant,dc=donvincenzo,dc=com");
+        if(ldap.createConnection(name,passwd) != true)fail("CreateConnexion");
+    }
+    
+    public void testGetAttribute()
+    {
+        String name="fappert";
+        String passwd = "";
+        FlexiLDAP ldap = new FlexiLDAP("gontime.ath.cx",389,"ou=Users,ou=Etudiant,dc=donvincenzo,dc=com","ou=Groups,ou=Etudiant,dc=donvincenzo,dc=com");
+        if(ldap.createConnection(name,passwd) != true)fail("CreateConnexion");
+        ArrayList list = new ArrayList();
+        if((list=ldap.getAttribute("cn",FlexiLDAP.TYPE_USER,"fappert"))==null)fail("Recuperation Attributs");
+        if( ((String)list.get(0)).compareTo("Fabien")!= 0 )fail("Lecture Attributs");
+    }
+}
+
