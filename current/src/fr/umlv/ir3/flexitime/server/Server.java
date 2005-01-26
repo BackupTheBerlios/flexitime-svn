412661d6121275070facf614991a6f3db7177253
valre <valre@42224c30-ede5-0310-ac12-a7a1f9b436d4>
2005-01-26 21:20:16 +0000

    MAJ de Server pour instancier UserManager

--- current/src/fr/umlv/ir3/flexitime/server/Server.java
+++ current/src/fr/umlv/ir3/flexitime/server/Server.java
@@ -5,14 +5,24 @@
  */
 package fr.umlv.ir3.flexitime.server;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.xml.sax.SAXException;
+
+import fr.umlv.ir3.flexitime.common.data.admin.IConfig;
+import fr.umlv.ir3.flexitime.common.data.admin.impl.ConfigImpl;
 import fr.umlv.ir3.flexitime.common.rmi.IDataManager;
+import fr.umlv.ir3.flexitime.common.rmi.admin.IUserManager;
 import fr.umlv.ir3.flexitime.server.core.DataManagerImpl;
+import fr.umlv.ir3.flexitime.server.core.admin.ConfigurationManager;
+import fr.umlv.ir3.flexitime.server.core.admin.UserManager;
 
 
 /**
@@ -43,10 +53,37 @@ public class Server
         Registry r = LocateRegistry.createRegistry(Registry.REGISTRY_PORT);
         
         IDataManager m = new DataManagerImpl();
+        IConfig ic = null;
+        try
+        {
+            ic = new ConfigurationManager().get();
+            //ic.setUriServerLDAP("ldapetud.univ-mlv.fr");
+            //ic.setPathGroupLDAP("ou=Groups,ou=Etudiant,dc=univ-mlv,dc=fr");
+            //ic.setPathUserLDAP("ou=Users,ou=Etudiant,dc=univ-mlv,dc=fr");
+            
+        }
+        catch (SAXException e1)
+        {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
+        catch (ParserConfigurationException e1)
+        {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
+        catch (IOException e1)
+        {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
+        
+        IUserManager u = new UserManager(ic);
         
         try
         {
             Naming.rebind("manager", m);
+            Naming.rebind("usermanager",u);
         }
         catch (RemoteException e)
         {
