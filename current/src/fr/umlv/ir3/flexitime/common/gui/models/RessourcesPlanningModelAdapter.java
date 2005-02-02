73eb3f9c281d8db654e638fcaaebd07d4608f6eb
binou <binou@42224c30-ede5-0310-ac12-a7a1f9b436d4>
2005-02-02 11:39:37 +0000

    debut adapter model multi ressources

--- current/src/fr/umlv/ir3/flexitime/common/gui/models/RessourcesPlanningModelAdapter.java
+++ current/src/fr/umlv/ir3/flexitime/common/gui/models/RessourcesPlanningModelAdapter.java
@@ -5,6 +5,13 @@
  */
 package fr.umlv.ir3.flexitime.common.gui.models;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import fr.umlv.ir3.flexitime.common.data.resources.IResource;
+import fr.umlv.ir3.flexitime.common.tools.Gap;
+
 
 /**
  * DOCME Description
@@ -20,6 +27,44 @@ package fr.umlv.ir3.flexitime.common.gui.models;
  */
 public class RessourcesPlanningModelAdapter
 {
+    
+    
+    private final List<IResource> ressourceList;
+    private List<RessourcePlanningModel> models;
+
+    /**
+     * DOCME
+     * @param ressourceList
+     * @param initialGap
+     */
+    public RessourcesPlanningModelAdapter(List<IResource> ressourceList, Gap initialGap)
+    {
+        this.ressourceList = ressourceList;
+        if(ressourceList == null)
+            System.err.println("RessourcesPlanningModelAdapter() : erreur de construction du model");
+        
+        this.models = new ArrayList<RessourcePlanningModel>(ressourceList.size());
+
+        
+        for(IResource ress : ressourceList)
+            models.add(new RessourcePlanningModel(ress, initialGap));
+    }
+    
+    /** 
+     * Return the element at the specified index<br>
+     *
+     * @param dayNumber the value of the day (0 is the first)
+     * @param gapNumber the value of the gap (0 is the first)
+     * @return the Object at the specified index
+     * 
+     * @author   FlexiTeam - binou
+     */
+    public Object getElementAt(int ressNumber, int blocNumber, int weekNumber, int dayNumber)
+    {
+        //TODO attention le gamNumber ne sera ptete pas le meme (BlocNumber en fait)
+        int gapNumber = blocNumber;
+        return models.get(ressNumber).getElementAt(weekNumber,dayNumber,gapNumber);
+    }
 
 }
 
